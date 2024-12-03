b1bfb774084e065fdcbc24f7e4d09152f34052ec
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2024-12-02 21:30:18 -0300

    feat: Add History page content & style

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,60 @@
+import { HistoryContainer, HistoryList } from './styles'
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>tarefa</td>
+              <td>20 min</td>
+              <td>Há 1 mês</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>20 min</td>
+              <td>Há 1 mês</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>20 min</td>
+              <td>Há 1 mês</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>20 min</td>
+              <td>Há 1 mês</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>20 min</td>
+              <td>Há 1 mês</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>20 min</td>
+              <td>Há 1 mês</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
+}
